ea54b529e2205a6eedb6fbebe8d30a4cf15304c4
brandon6190 <brandon6190@outlook.com>
2019-02-24 15:22:47 -0500

    created script to hide ads from keybr.com

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,15 +1,11 @@
-// this is the code which will be injected into a given page...
+// this is the code which will be injected into keybr.com ...
 
 (function() {
 
-	// just place a div at top right
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected!';
-	document.body.appendChild(div);
+  let asideAd = document.querySelector('.Body-aside');
+  let bodyAd = document.querySelector('.Body-header');
 
-	alert('inserted self... giggity');
+  asideAd.style.visibility = 'hidden';
+  bodyAd.style.visibility = 'hidden';
 
 })();
