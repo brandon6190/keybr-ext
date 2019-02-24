9589cd9f5b32bb7a6523c8194892b67eb8f5572b
brandon6190 <brandon6190@outlook.com>
2019-02-24 14:41:23 -0500

    added sample script to inject, for testing

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,15 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+// this is the code which will be injected into a given page...
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+(function() {
 
-	}
-	}, 10);
-});
+	// just place a div at top right
+	var div = document.createElement('div');
+	div.style.position = 'fixed';
+	div.style.top = 0;
+	div.style.right = 0;
+	div.textContent = 'Injected!';
+	document.body.appendChild(div);
+
+	alert('inserted self... giggity');
+
+})();
